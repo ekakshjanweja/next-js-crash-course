083009022832ade0d0f8249ee81ff836d40fe254
Ekaksh Janweja <jekaksh@gmail.com>
2023-06-12 15:30:51 +0530

    routes & nested routes

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div className="flex justify-center mt-20">
+      <p className="text-3xl hover:text-blue-500 font-mono font-semibold">Personal Blog</p>
+    </div>
+  );
+}
