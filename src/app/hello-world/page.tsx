88513cee9ef6a65046dce1b8d05f35868f1995fd
Ekaksh Janweja <jekaksh@gmail.com>
2023-06-12 15:17:43 +0530

    Hello, World!

---
+++ src/app/hello-world/page.tsx
@@ -0,0 +1,7 @@
+export default function HelloWorld() {
+  return (
+    <div className="flex justify-center mt-20">
+      <p className="text-3xl hover:text-purple-500 font-mono"> Hello, World!</p>
+    </div>
+  );
+}
